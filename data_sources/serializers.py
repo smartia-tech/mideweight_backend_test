23ba5f0751602188f51ef9346424a4892fc4e973
Parsa <parsa_ghorbani@outlook.com>
2021-02-19 17:58:41 +0330

    Fix serializers

--- data_sources/serializers.py
+++ data_sources/serializers.py
@@ -10,33 +10,12 @@ class PosseSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class GatewaySerializer(serializers.ModelSerializer):
-    tags = GatewayTagSerialzer(many=True, read_only=True)
-    data_flow = serializers.BooleanField()
-
-    class Meta:
-        model = Gateway
-        fields = ('label', 'posse', 'location', 'serial_number', 'type_name', 'tags', 'data_flow')
-
-
-class GatewayStatusSerializer(serializers.ModelSerializer):
-    gateway = serializers.HypelinkedRelatedField(
-        many=False,
-        read_only=True,
-        view_name='gateway'
-    )
-
-    class Meta:
-        model = GatewayStatus
-        fields = '__all__'
-
-
 # There must a hard way to abstract datasource serializing but
 # for now I don't think it worth doing unless the project may add many tags
 # and datasource each week or two if that's the case we may be able to do some dirty work
 # with automatic code replacement or something like that    
-class GatewayTagSerialzer(serializers.ModelSerializer):
-    gateway = serializers.HypelinkedRelatedField(
+class GatewayTagSerializer(serializers.ModelSerializer):
+    gateway = serializers.HyperlinkedRelatedField(
         many=False,
         read_only=True,
         view_name='gateway'
@@ -45,3 +24,24 @@ class GatewayTagSerialzer(serializers.ModelSerializer):
     class Meta:
         model = GatewayTag
         fields = "__all__"
+
+
+class GatewaySerializer(serializers.ModelSerializer):
+    tags = GatewayTagSerializer(many=True, read_only=True)
+    data_flow = serializers.BooleanField()
+
+    class Meta:
+        model = Gateway
+        fields = ('label', 'posse', 'location', 'serial_number', 'type_name', 'tags', 'data_flow')
+
+
+class GatewayStatusSerializer(serializers.ModelSerializer):
+    gateway = serializers.HyperlinkedRelatedField(
+        many=False,
+        read_only=True,
+        view_name='gateway'
+    )
+
+    class Meta:
+        model = GatewayStatus
+        fields = '__all__'
