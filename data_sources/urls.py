8126f6c7ccc3e18cb82e56e49cb791d77976e13b
Parsa <parsa_ghorbani@outlook.com>
2021-02-19 18:20:54 +0330

    Add routes file to cleanse urls

--- data_sources/urls.py
+++ data_sources/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path, include
 
-from data_sources.views import GatewaysView
+from data_sources.routes import *
 
-gateway_list = GatewaysView.as_view({
-    'get': 'list'
-})
-
-gateway_retrive = GatewaysView.as_view({
-    'get': 'retrieve'
-})
 
 urlpatterns = [
+    path('posses/', posse_list, name='posse-list'),
+    path('posses/<int:pk>/', posse_retrieve, name='posse'),
     path('gateways/', gateway_list, name='gateway-list'),
-    path('gateways/<int:pk>/', gateway_retrive, name='gateway')
+    path('gateways/<int:pk>/', gateway_retrieve, name='gateway'),
+    path('gateway_tags/', gateway_tag_list, name='gateway-tag-list'),
+    path('gateway_tags/<int:pk>/', gateway_tag_retrieve, name='gateway-tag'),
+    path('gateway_statuses/', gateway_status_list, name='gateway-status-list'),
+    path('gateway_statuses/<int:pk>', gateway_status_retrieve, name='gateway-status'),
 ]
