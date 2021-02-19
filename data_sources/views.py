e126a5fcb106617afed1cedbe92d086a474eed02
Parsa <parsa_ghorbani@outlook.com>
2021-02-19 18:21:30 +0330

    Add rest of views

--- data_sources/views.py
+++ data_sources/views.py
@@ -3,10 +3,27 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Gateway
-from .serializers import GatewaySerializer
+from data_sources.models import Posse, Gateway, GatewayTag, GatewayStatus
+from data_sources.serializers import (
+    PosseSerializer, GatewaySerializer, GatewayTagSerializer, GatewayStatusSerializer
+)
 
 
-class GatewaysView(ModelViewSet):
+class PosseView(ModelViewSet):
+    queryset = Posse.objects.all()
+    serializer_class = PosseSerializer
+
+
+class GatewayView(ModelViewSet):
     queryset = Gateway.objects.all()
     serializer_class = GatewaySerializer
+
+
+class GatewayTagsView(ModelViewSet):
+    queryset = GatewayTag.objects.all()
+    serializer_class = GatewayTagSerializer
+
+
+class GatewayStatusView(ModelViewSet):
+    queryset = GatewayStatus.objects.all()
+    serializer_class = GatewayStatusSerializer
