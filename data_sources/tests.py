42fdb073426bdced257ae7fae8304a4eae2ac6ad
Raheem Azeez Abiodun <raheemazeez4@gmail.com>
2021-02-20 09:08:56 +0100

    Gateway status

--- data_sources/tests.py
+++ data_sources/tests.py
@@ -2,8 +2,9 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from data_sources.serializers import PosseSerializer, GatewaySerializer
-from data_sources.models import Posse, Gateway
+from data_sources.serializers import PosseSerializer, GatewaySerializer, GatewayStatusSerializer,\
+    ReferenceGatewayStatusSerializer, ReferenceBaseGatewaySerializer
+from data_sources.models import Posse, Gateway, GatewayStatus
 
 
 class TestPosse(TestCase):
@@ -32,32 +33,58 @@ class TestPosse(TestCase):
 
 class TestGatewayView(TestCase):
     def setUp(self):
-        self.url = '/api/v1/gateway/'
+        self.gateway_url = '/api/v1/gateway/'
+        self.gateway_status_url = '/api/v1/gateway-status/'
         self.client = APIClient()
 
-        self.posse = posse=Posse.objects.create(label='Posse Label 1')
-        Gateway.objects.create(label="Gateway 1",
-                               posse=self.posse,
-                               location="London",
-                               oauth2_client_id="some-random-string",
-                               serial_number="dshjsd-89323")
+        self.posse = Posse.objects.create(label='Posse Label 1')
+        self.gateway = Gateway.objects.create(label="Gateway 1",
+                                              posse=self.posse,
+                                              location="London",
+                                              oauth2_client_id="some-random-string",
+                                              serial_number="dshjsd-89323")
+        self.gateway_status = GatewayStatus.objects.create(gateway=self.gateway,
+                                                           hostname='hostname',
+                                                           data_flow=True,
+                                                           os_name='Macintosh',
+                                                           os_version='catalina',
+                                                           firmware_version='23923-v',
+                                                           maio_edge_version='maybe version 2')
 
     def test_gateway(self):
-        response = self.client.get(path=self.url, HTTP_AUTHORIZATION='Bearer slbnkmdysqpxyzwacwpztjfikptx')
+        response = self.client.get(path=self.gateway_url, HTTP_AUTHORIZATION='Bearer slbnkmdysqpxyzwacwpztjfikptx')
 
         gateway = Gateway.objects.all()
         serializer = GatewaySerializer(gateway, many=True)
         self.assertEqual(response.data['results'], serializer.data)
         
         # since the request is not verbose, test for it
-        self.assertEqual(response.data['results'][0]['posse'], 1)
+        self.assertEqual(response.data['results'][0]['posse'], self.posse.id)
+        self.assertEqual(response.data['results'][0]['status'], [self.gateway_status.id])
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # test verbose request
-        response = self.client.get(path='{}?response=verbose'.format(self.url),
+        response = self.client.get(path='{}?response=verbose'.format(self.gateway_url),
                                    HTTP_AUTHORIZATION='Bearer slbnkmdysqpxyzwacwpztjfikptx')
 
-        gateway = Gateway.objects.all()
-        serializer = GatewaySerializer(gateway, many=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['results'][0]['posse'], PosseSerializer(self.posse).data)
+        self.assertEqual(response.data['results'][0]['status'], ReferenceGatewayStatusSerializer([self.gateway_status], many=True).data)
+
+        ###### test gateway status
+        response = self.client.get(path=self.gateway_status_url,
+                                   HTTP_AUTHORIZATION='Bearer slbnkmdysqpxyzwacwpztjfikptx')
+
+        gateway_status = GatewayStatus.objects.all()
+        serializer = GatewayStatusSerializer(gateway_status, many=True)
+        self.assertEqual(response.data['results'], serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'][0]['gateway'], self.gateway.id)
+
+
+        # verbose request
+        response = self.client.get(path='{}?response=verbose'.format(self.gateway_status_url),
+                                   HTTP_AUTHORIZATION='Bearer slbnkmdysqpxyzwacwpztjfikptx')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'][0]['gateway'], ReferenceBaseGatewaySerializer(self.gateway).data)
