5695cc3ad44aff48ed8a9009d70c42d0b12aa83b
Emmanuel <chinedue16@gmail.com>
2021-02-21 01:09:46 +0100

    Feat: :snake: Added Admin

--- data_sources/admin.py
+++ data_sources/admin.py
@@ -1,31 +1,45 @@
-# from django.contrib import admin
-# from .models import (
-#     Posse, Gateway, GatewayStatus, GatewayTag
-# )
+from django.contrib import admin
+from .models import (
+    Posse, Gateway, GatewayStatus, GatewayTag
+)
 
 
-# class PosseAdmin(admin.ModelAdmin):
-#     list_display = "__all__"
+class PosseAdmin(admin.ModelAdmin):
+    list_display = [
+        "label"
+    ]
 
 
-# admin.site.register(Posse, PosseAdmin)
-
-# class GatewayAdmin(admin.ModelAdmin):
-#     list_display = "__all__"
+admin.site.register(Posse, PosseAdmin)
 
 
-# admin.site.register(Gateway, GatewayAdmin)
+class GatewayAdmin(admin.ModelAdmin):
+    llist_display = [
+        "label", "posse", "location",
+        "oauth2_client_id", "serial_number",
+        "type_name"
+    ]
 
 
-# class GatewayStatusAdmin(admin.ModelAdmin):
-#     list_display = "__all__"
+admin.site.register(Gateway, GatewayAdmin)
 
 
-# admin.site.register(GatewayStatus, GatewayStatusAdmin)
+class GatewayStatusAdmin(admin.ModelAdmin):
+    list_display = [
+        "gateway", "hostname", "data_flow",
+        "os_name", "os_version", "firmware_version",
+        "maio_edge_version", "created_at"
+    ]
 
 
-# class GatewayTagAdmin(admin.ModelAdmin):
-#     list_display = "__all__"
+admin.site.register(GatewayStatus, GatewayStatusAdmin)
 
 
-# admin.site.register(GatewayTag, GatewayTagAdmin)
+class GatewayTagAdmin(admin.ModelAdmin):
+    list_display = [
+        "gateway", "data_flow", "hardware_name",
+        "unit_name", "unit_type", "status"
+    ]
+
+
+admin.site.register(GatewayTag, GatewayTagAdmin)
