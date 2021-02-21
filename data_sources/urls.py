297981c2df7d068dbacba0640420efc0289a4e27
Popoola Boluwatife <olaoyejnr@gmail.com>
2021-02-21 02:32:39 +0000

    serialize and expose models

--- data_sources/urls.py
+++ data_sources/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path
-
+from data_sources.views import (
+    PosseView, PosseDetailView,
+    GatewayView, GatewayDetailView,
+    GatewayStatusView, GatewayStatusDetailView,
+    GatewayTagView, GatewayTagDetailView
+)
 urlpatterns = [
-    
+    path('posses/', PosseView.as_view(), name='posse-list'),
+    path('posses/<int:pk>/', PosseDetailView.as_view(), name='posse-detail'),
+    path('gateways/', GatewayView.as_view(), name='gateway-list'),
+    path('gateways/<int:pk>/', GatewayDetailView.as_view(), name='gateway-detail'),
+    path('gatewaystatus/', GatewayStatusView.as_view(), name='gatewaystatus-list'),
+    path('gatewaystatus/<int:pk>/', GatewayStatusDetailView.as_view(), name='gatewaystatus-detail'),
+    path('gatewaytag/', GatewayTagView.as_view(), name='gatewaytag-list'),
+    path('gatewaytag/<int:pk>/', GatewayTagDetailView.as_view(), name='gatewaytag-detail'),
 ]
--- data_sources/views.py
+++ data_sources/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from data_sources.models import (
+    Posse, Gateway,
+    GatewayStatus, GatewayTag
+)
+from data_sources.serializers import (
+    PosseSerializer, GatewaySerializer,
+    GatewayStatusSerializer, GatewayTagSerializers
+)
 
-# Create your views here.
+
+class PosseView(ListAPIView):
+    serializer_class = PosseSerializer
+    queryset = Posse.objects.all()
+
+
+class PosseDetailView(RetrieveAPIView):
+    serializer_class = PosseSerializer
+    queryset = Posse.objects.all()
+
+
+class GatewayView(ListAPIView):
+    serializer_class = GatewaySerializer
+    queryset = Gateway.objects.all()
+
+
+class GatewayDetailView(RetrieveAPIView):
+    serializer_class = GatewaySerializer
+    queryset = Gateway.objects.all()
+
+
+class GatewayStatusView(ListAPIView):
+    serializer_class = GatewayStatusSerializer
+    queryset = GatewayStatus.objects.all()
+
+
+class GatewayStatusDetailView(RetrieveAPIView):
+    serializer_class = GatewayStatusSerializer
+    queryset = GatewayStatus.objects.all()
+
+
+class GatewayTagView(ListAPIView):
+    serializer_class = GatewayTagSerializers
+    queryset = GatewayTag.objects.all()
+
+
+class GatewayTagDetailView(RetrieveAPIView):
+    serializer_class = GatewayTagSerializers
+    queryset = GatewayTag.objects.all()
