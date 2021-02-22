329417d7f5feb0b771a151e77ee77a86c6c8a56d
techstack1611 <techstack1611@gmail.com>
2021-02-22 19:27:05 +0530

    Feat: Create CRUD APIs to expose the models in data sources
    - Poss CRUD APIs
    - Gateway CRUD APIs
    - Gateway Status CRUD APIs
    - Gateway Tag CRUD APIs
    - Admin panel for better
    - Customized django admin dashboard

--- data_sources/views.py
+++ data_sources/views.py
@@ -1,3 +1,124 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Posse, Gateway, GatewayStatus, GatewayTag
+from .serializers import PosseSerializer, GatewaySerializer, GatewayStatusSerializer, GatewayTagSerializer
+
+
+class PosseList(generics.ListCreateAPIView):
+    """
+    get:
+    Return a list of all the existing posse.
+
+    post:
+    Create a new posse instance.
+    """
+    queryset = Posse.objects.all()
+    serializer_class = PosseSerializer
+
+
+class PosseDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    get:
+    Return a specific posse instance.
+
+    put:
+    Update the the specific posse instance.
+
+    patch:
+    Partial update the specific posse instance.
+
+    delete:
+    Delete the the specific posse instance.
+    """
+    queryset = Posse.objects.all()
+    serializer_class = PosseSerializer
+
+
+class GatewayList(generics.ListCreateAPIView):
+    """
+    get:
+    Return a list of all the existing gateway.
+
+    post:
+    Create a new gateway instance.
+    """
+    queryset = Gateway.objects.all()
+    serializer_class = GatewaySerializer
+
+
+class GatewayDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    get:
+    Return a specific gateway instance.
+
+    put:
+    Update the the specific gateway instance.
+
+    patch:
+    Partial update the specific gateway instance.
+
+    delete:
+    Delete the the specific gateway instance.
+    """
+    queryset = Gateway.objects.all()
+    serializer_class = GatewaySerializer
+
+
+class GatewayStatusList(generics.ListCreateAPIView):
+    """
+    get:
+    Return a list of all the existing gateway status.
+
+    post:
+    Create a new gateway status instance.
+    """
+    queryset = GatewayStatus.objects.all()
+    serializer_class = GatewayStatusSerializer
+
+
+class GatewayStatusDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    get:
+    Return a specific gateway status instance.
+
+    put:
+    Update the the specific gateway status instance.
+
+    patch:
+    Partial update the specific gateway status instance.
+
+    delete:
+    Delete the the specific gateway status instance.
+    """
+    queryset = GatewayStatus.objects.all()
+    serializer_class = GatewayStatusSerializer
+
+
+class GatewayTagList(generics.ListCreateAPIView):
+    """
+    get:
+    Return a list of all the existing gateway tag.
+
+    post:
+    Create a new gateway tag instance.
+    """
+    queryset = GatewayTag.objects.all()
+    serializer_class = GatewayTagSerializer
+
+
+class GatewayTagDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    get:
+    Return a specific gateway tag instance.
+
+    put:
+    Update the the specific gateway tag instance.
+
+    patch:
+    Partial update the specific gateway tag instance.
+
+    delete:
+    Delete the the specific gateway tag instance.
+    """
+    queryset = GatewayTag.objects.all()
+    serializer_class = GatewayTagSerializer
