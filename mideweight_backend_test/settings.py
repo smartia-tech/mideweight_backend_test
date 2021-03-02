7860500250aec0b941f0ea8b1cd7b7063e63ff48
Umesh Chaudhary <umesschaudhary@gmail.com>
2021-03-03 00:31:06 +0545

    add isauthenticated setting for rest api

--- mideweight_backend_test/settings.py
+++ mideweight_backend_test/settings.py
@@ -133,6 +133,9 @@ STATIC_URL = '/static/'
 # Rest Framework Configuration
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
