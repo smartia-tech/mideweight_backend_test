ddccbc86e773725a5e280ef2c97a20eccb0b2a05
Umesh Chaudhary <umesschaudhary@gmail.com>
2021-03-03 00:28:01 +0545

    add filters, search & ordering on apis

--- data_sources/api/v1/views.py
+++ data_sources/api/v1/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from data_sources.api.v1.serializers import (
     GatewayGetSerializer, GatewaySerializer,
@@ -14,11 +16,30 @@ from data_sources.models import (
 class PosseViewSet(viewsets.ModelViewSet):
     queryset = Posse.objects.all()
     serializer_class = PosseSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    search_fields = ['label']
 
 
 class GatewayViewSet(viewsets.ModelViewSet):
     queryset = Gateway.objects.all()
     serializer_class = GatewayGetSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+    search_fields = [
+        'label',
+        'posse__label',
+        'location',
+        'oauth2_client_id',
+        'serial_number',
+        'type_name'
+    ]
+    filter_fields = [
+        'posse',
+    ]
+
+    ordering_fields = [
+        'created_at',
+        'updated_at'
+    ]
 
     def get_serializer_class(self):
         serializer = super().get_serializer_class()
@@ -30,6 +51,23 @@ class GatewayViewSet(viewsets.ModelViewSet):
 class GatewayTagViewSet(viewsets.ModelViewSet):
     queryset = GatewayTag.objects.all()
     serializer_class = GatewayTagGetSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+    search_fields = [
+        'gateway__label',
+        'hardware_name',
+        'unit_name',
+    ]
+    filter_fields = [
+        'gateway',
+        'unit_type',
+        'status',
+        'data_flow'
+    ]
+
+    ordering_fields = [
+        'created_at',
+        'updated_at'
+    ]
 
     def get_serializer_class(self):
         serializer = super().get_serializer_class()
@@ -41,6 +79,22 @@ class GatewayTagViewSet(viewsets.ModelViewSet):
 class GatewayStatusViewSet(viewsets.ModelViewSet):
     queryset = GatewayStatus.objects.all()
     serializer_class = GatewayStatusGetSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+    search_fields = [
+        'gateway__label',
+        'hostname',
+        'os_name',
+        'firmware_version',
+        'maio_edge_version',
+    ]
+    filter_fields = [
+        'gateway',
+        'data_flow'
+    ]
+
+    ordering_fields = [
+        'created_at',
+    ]
 
     def get_serializer_class(self):
         serializer = super().get_serializer_class()
