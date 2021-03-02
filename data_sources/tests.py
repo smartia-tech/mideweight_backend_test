e1f6350bd004cb2374d74527a4a83948099850ca
Umesh Chaudhary <umesschaudhary@gmail.com>
2021-03-03 01:01:40 +0545

    add test cases for gateway

--- data_sources/tests.py
+++ data_sources/tests.py
@@ -14,6 +14,9 @@ User = get_user_model()
 
 
 class UserCreate:
+    """
+    A user creation helper class
+    """
 
     def create(self):
         user = User.objects.create(email="admin@gmail.com", username="admin")
@@ -23,6 +26,9 @@ class UserCreate:
 
 
 class ProtectedAPITestCase(APITestCase):
+    """
+    Logins the user
+    """
 
     def setUp(self):
         user = UserCreate().create()
@@ -34,45 +40,91 @@ class ProtectedAPITestCase(APITestCase):
 
 
 class PosseTestcase(ProtectedAPITestCase):
-    '''
-    some test cases for posse
-    '''
+    """
+    Test cases for Posse
+    """
 
     def setUp(self):
         super().setUp()
-        self.posse = Posse.objects.create(label='Nepali')
+        self.posse = Posse.objects.create(label='possee')
 
     def test_create(self):
         """
         post posse
-
         """
 
         post_url = reverse('api_v1:posse-list')
         data = {
             'label': "Hey"
         }
-        response = self.client.post(
-            post_url,
-            data=data,
-            format='json'
-        )
+        response = self.client.post(post_url, data=data, format='json'
+                                    )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_list(self):
-        '''
-        get list of posse
-        '''
+        """test list of posse"""
+
         get_url = reverse('api_v1:posse-list')
         response = self.client.get(get_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_detail(self):
-        '''
-        test enpoint for getting specific posses
-        '''
+        """test detail of posse"""
+
         get_url = reverse('api_v1:posse-detail', args=(self.posse.pk,))
         response = self.client.get(get_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['id'], self.posse.pk)
-        self.assertEqual(response.data['label'], 'Nepali')
+        self.assertEqual(response.data['label'], 'possee')
+
+
+class GatewayTestcase(ProtectedAPITestCase):
+    """
+    Test cases for Gateway
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.posse = Posse.objects.create(label="Possee")
+        self.gateway = Gateway.objects.create(
+            label='Gateway', posse=self.posse,
+            location="Nepal", oauth2_client_id="1",
+            serial_number="12345"
+        )
+
+    def test_create(self):
+        """
+        post posse
+        """
+
+        post_url = reverse('api_v1:gateway-list')
+        data = {
+            'label': "Hey",
+            'posse': self.posse.pk,
+            'location': 'Nepal',
+            'oauth2_client_id': '123455',
+            'serial_number': '012324334'
+        }
+        response = self.client.post(post_url, data=data, format='json'
+                                    )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_list(self):
+        """test list of gateway"""
+
+        get_url = reverse('api_v1:gateway-list')
+        response = self.client.get(get_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_detail(self):
+        """
+        get detail of gateway
+        """
+
+        get_url = reverse('api_v1:gateway-detail', args=(self.gateway.pk,))
+        response = self.client.get(get_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], self.posse.pk)
+        self.assertEqual(response.data['label'], 'Gateway')
+        self.assertEqual(response.data['serial_number'], '12345')
+        self.assertEqual(response.data['location'], 'Nepal')
