7be6c0f686e56daf7799d8142e8238fc86f93313
Henrique Kubenda <henrique.kubenda@gmail.com>
2021-03-03 23:46:00 +0000

    add data_souces models to admin page just for development ease

--- data_sources/admin.py
+++ data_sources/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Gateway,
+    GatewayStatus,
+    GatewayTag,
+    Posse,
+)
+
+admin.site.register([
+    Gateway,
+    GatewayStatus,
+    GatewayTag,
+    Posse,
+])
